f451d11d40880ba26da535a522fc749388d93dec
atodadson <benjamindadson32@gmail.com>
2025-03-02 07:34:10 +0000

    Multiple inputs

--- userinput.go
+++ userinput.go
@@ -3,8 +3,16 @@ package main
 import "fmt"
 
 // Reading simple user inputs
+// func main() {
+//     var response string
+//     fmt.Scan(&response)
+//     fmt.Println("User typed: " + response)
+// }
+
+// Reading mutlitple user inputs
 func main() {
-    var response string
-    fmt.Scan(&response)
-    fmt.Println("User typed: " + response)
+    var firstname, lastname string
+    fmt.Println("Enter your firstname and lastname")
+    fmt.Scan(&firstname, &lastname)
+    fmt.Printf("Hello Mr./Mrs. %s %s", firstname, lastname )
 }
