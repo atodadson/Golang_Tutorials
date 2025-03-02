459651b2dba5d0f6e245959acf1840f63332f161
atodadson <benjamindadson32@gmail.com>
2025-03-02 01:44:37 +0000

    False while loops in go

--- loops.go
+++ loops.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 // Basic For loops. Starting point and increment upto a limit
-func main() {
-    for i := 0; i < 10; i++ {
-        fmt.Println(i)
-    }
+// func main() {
+//     for i := 0; i < 10; i++ {
+//         fmt.Println(i)
+//     }
+// }
 
+// The Go language does not a keyword for while. Instead it uses a modified for loop
+func main() {
+    i := 0
+    for i < 10 {
+        i = i + 1
+        fmt.Printf("The current value of i is: %d\n", i)
+    }
 }
