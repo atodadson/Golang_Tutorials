0fc541db09db4f60d93d9efcc7ccac74f9cf1651
atodadson <benjamindadson32@gmail.com>
2025-03-02 01:58:24 +0000

    For each in go using range

--- loops.go
+++ loops.go
@@ -2,18 +2,28 @@ package main
 
 import "fmt"
 
-// Basic For loops. Starting point and increment upto a limit
+// 1. Basic For loops. Starting point and increment upto a limit
 // func main() {
 //     for i := 0; i < 10; i++ {
 //         fmt.Println(i)
 //     }
 // }
 
-// The Go language does not a keyword for while. Instead it uses a modified for loop
+// 2. The Go language does not a keyword for while. Instead it uses a modified for loop
+// func main() {
+//     i := 0
+//     for i < 10 {
+//         i = i + 1
+//         fmt.Printf("The current value of i is: %d\n", i)
+//     }
+// }
+
+// 3. The for each structure
 func main() {
-    i := 0
-    for i < 10 {
-        i = i + 1
-        fmt.Printf("The current value of i is: %d\n", i)
+    var cars = [4]string{"volvo", "bugatti", "benz", "bentley"}
+
+    for index, item := range cars {
+        fmt.Printf("Index: %d, item: %s\n", index ,item)
     }
+
 }
