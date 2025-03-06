d6557ac6a8082e200f4eea99a57ed267e22acc4c
atodadson <benjamindadson32@gmail.com>
2025-03-06 22:44:34 +0000

    defining errors

--- handling_errors.go
+++ handling_errors.go
@@ -2,6 +2,7 @@ package main
 
 import (
 "fmt"
+"errors"
 )
 
 // Using recover and defer
@@ -20,7 +21,22 @@ func Divide(dividend float64, divisor float64) (answer float64) {
     return dividend / divisor
 }
 
+// runtime/debug and error logging
+
+
+// Defining errors
+var NumTooSmall = errors.New("Expected a number greater than zero")
+
+func ReturnPositive(no int) (int, error) {
+    if no > 0 {
+        return no, nil
+    } else {
+        return 0, NumTooSmall
+    }
+}
 
 func main() {
     Divide(20, 0)
+    num, err := ReturnPositive(12)
+    fmt.Printf("Num: %v, Error: %v", num, err)
 }
