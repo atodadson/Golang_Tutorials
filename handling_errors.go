4ca5b29b59bd772ba5c0b22be5ddde43524c9c7a
atodadson <benjamindadson32@gmail.com>
2025-03-06 22:14:13 +0000

    defer and recover

--- handling_errors.go
+++ handling_errors.go
@@ -1,17 +1,25 @@
 package main
 
-// import (
-// "fmt"
-// )
+import (
+"fmt"
+)
+
+// Using recover and defer
+func ErrorHandler() {
+    if r := recover(); r != nil {
+        fmt.Printf("Recovering from error: %v \n", r)
+    }
+}
 
 // Using panic and recover
-func Divide(first float64, second float64) (answer float64) {
-    if second == 0 {
+func Divide(dividend float64, divisor float64) (answer float64) {
+    defer ErrorHandler()
+    if divisor == 0 {
         panic("Can't Divide by 0")
     }
-    return first / second
-
+    return dividend / divisor
 }
+
 
 func main() {
     Divide(20, 0)
