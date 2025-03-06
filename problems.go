59fba1fc3ad6b223418cc3fdbcc831deb3284d8d
atodadson <benjamindadson32@gmail.com>
2025-03-06 13:39:51 +0000

    Refactored

--- problems.go
+++ problems.go
@@ -7,29 +7,29 @@ package main
 
 import "fmt"
 
-func main() {
-    num := 6
-    unsolved := true
-    big := 1
-    for unsolved {
-        num++
-        fmt.Printf("Current num: %d -- ", num)
-        for i := 2; i <= 6; i++ {
-            var mod int
-            mod = num % i
-            if mod != 1 {
-                fmt.Printf("%d failed, mod = %d \n", i, mod)
-                break
-            } else {
-                big = mod
-            }
-        if (big == 6 && (num % 7) == 0) {
-            fmt.Printf("The answer is %d:", num)
-            unsolved = false
-        }
-        }
-    }
-}
+// func main() {
+//     num := 6
+//     unsolved := true
+//     big := 1
+//     for unsolved {
+//         num++
+//         fmt.Printf("Current num: %d -- ", num)
+//         for i := 2; i <= 6; i++ {
+//             var mod int
+//             mod = num % i
+//             if mod != 1 {
+//                 fmt.Printf("%d failed, mod = %d \n", i, mod)
+//                 break
+//             } else {
+//                 big = mod
+//             }
+//         if (big == 6 && (num % 7) == 0) {
+//             fmt.Printf("The answer is %d:", num)
+//             unsolved = false
+//         }
+//         }
+//     }
+// }
 
 // func main() {
 //     num := 7
@@ -55,18 +55,18 @@ func main() {
 //     }
 // }
 
-// func main() {
-//     num := 1
-//     unsolved := true
-//     for unsolved {
-//         num += 60
-//         fmt.Printf("Current num: %d -- ", num)
-//         mod := (num % 7)
-//         if mod == 0 {
-//             fmt.Printf("The answer is %d:", num)
-//             unsolved = false
-//         } else {
-//             fmt.Printf("failed with mod : %d \n", mod)
-//         }
-//     }
-// }
+func main() {
+    num := 1
+    unsolved := true
+    for unsolved {
+        num += 60
+        fmt.Printf("Current num: %d -- ", num)
+        mod := (num % 7)
+        if mod == 0 {
+            fmt.Printf("The answer is %d:", num)
+            unsolved = false
+        } else {
+            fmt.Printf("failed with mod : %d \n", mod)
+        }
+    }
+}
--- userinput.go
+++ userinput.go
@@ -10,9 +10,38 @@ import "fmt"
 // }
 
 // Reading mutlitple user inputs
+// func main() {
+//     var firstname, lastname string
+//     fmt.Println("Enter your firstname and lastname")
+//     fmt.Scan(&firstname, &lastname)
+//     fmt.Printf("Firstname: %s Lastname: %s", firstname, lastname )
+// }
+
+/*
+Try modifying the program so it takes several players first and then ensures all players get a name.
+Here’s an example output of such a program:
+
+1 Enter number of players:
+2 3
+3 Enter Player 1 name:
+4 Alice
+5 Enter Player 2 name:
+6 Bob
+7 Enter Player 3 name:
+8 Jean
+9 Players are: Alice Bob Jean
+*/
+
 func main() {
-    var firstname, lastname string
-    fmt.Println("Enter your firstname and lastname")
-    fmt.Scan(&firstname, &lastname)
-    fmt.Printf("Firstname: %s Lastname: %s", firstname, lastname )
+    fmt.Println("Enter number of players")
+    var num_of_players int
+    fmt.Scanf("%d", &num_of_players)
+    var names string
+    for i := 1; i <= num_of_players; i++ {
+        var name string
+        fmt.Printf("Enter Player %d name: \n", i)
+        fmt.Scanf("%s", &name)
+        names = names + " " + name
+    }
+    fmt.Printf("Names of players are: %v", names)
 }
