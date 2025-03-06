ecd72ff8228b0d3bc16db0eff4a8c39798ba767a
atodadson <benjamindadson32@gmail.com>
2025-03-06 14:59:17 +0000

    Functions with returns

--- functions.go
+++ functions.go
@@ -2,11 +2,24 @@ package main
 
 import "fmt"
 
-// Functions with no parameters but a return
+// Functions with no parameters with no return
 func greet() {
     fmt.Println("Hello world! ")
 }
 
+// Functions with parameters and a return case 1: Unnamed return but return type specified
+func add(first int, second int) int {
+    return (first + second)
+}
+
+
+func mod(first int, second int) (remainder int) {
+    remainder = first % second
+    return
+}
+
 func main() {
     greet()
+    result := add(20, 5)
+    fmt.Printf("result is %v", result)
 }
