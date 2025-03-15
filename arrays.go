ef47024729b95da54d33540255d6080898f9b2e6
atodadson <benjamindadson32@gmail.com>
2025-03-15 08:24:28 +0000

    Using copy to make slices

--- arrays.go
+++ arrays.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+
 func main() {
     myArray := []string{"pineapple", "orange", "audi", "bugatti", "mango", "pawpaw", "mutton", "beef", "chicken"}
     capacity := cap(myArray)
@@ -17,5 +18,16 @@ func main() {
 
     fmt.Printf("Array: %v, Length: %v, Cap is %v \n" ,myArray, length, capacity)
     fmt.Printf("Array: %v, Cars: %v, Fruits is %v \n" ,myArray, cars, fruits)
+
+    // Creating Slices with make()
+    slice := make([]int, 3, 5) // Create array make(type, length, capacity)
+    fmt.Printf("Slice: %v ", slice)
+
+    // Making copies
+    fruits_copy := make([]string, 8)
+    copy(fruits_copy, fruits[0:3])
+    fmt.Printf("Fruits_copy: %v ", fruits_copy)
+
+    logger()
 
 }
