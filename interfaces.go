80ade6c16e6e52374a1250124dde787d9692bc6e
atodadson <benjamindadson32@gmail.com>
2025-03-15 13:21:16 +0000

    interface basic implementation

--- interfaces.go
+++ interfaces.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+// Implementation of interfaces
+type Rectangle struct {
+    length float32
+    width float32
+}
+
+func (r Rectangle) area() float32 {
+    return r.length * r.width
+}
+
 func main() {
     // While struct defines an object, interfaces defines behaviour
     // Declaration of interfaces
@@ -14,5 +24,8 @@ func main() {
         area() float32
         location() Point
     }
+
+   myRect := Rectangle{20, 15}
+   fmt.Printf("Area of rectangle with sides %v and %v is: %v", myRect.length, myRect.width, myRect.area())
 
 }
