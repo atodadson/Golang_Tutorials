930c51571fa21a0ac53389ab0b5d2e99563bd5f9
atodadson <benjamindadson32@gmail.com>
2025-03-15 09:20:54 +0000

    structs definition

--- structs.go
+++ structs.go
@@ -11,13 +11,17 @@ func main() {
         id int32
     }
 
-    // Creating an instance of a struct
+    // Creating an instance of a struct - Defining individual attributes
     var myAccount Account
     myAccount.firstname = "Bernard"
     myAccount.lastname = "Darko"
     myAccount.balance = 35.45
     myAccount.id = 20
 
-    fmt.Printf("Account Details: %v", myAccount)
+    // Creating an instance of a struct - Defining at once
+    hisAccount := Account{"Kofi", "Manu", 1234.43, 12}
+
+    fmt.Printf("Account Details: %v \n", myAccount)
+    fmt.Printf("Account Details: %v", hisAccount)
 
 }
