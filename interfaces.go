879c86ee956e0c30e4cd7e0faa3d2cca25e6917f
atodadson <benjamindadson32@gmail.com>
2025-03-15 13:27:19 +0000

    refactored

--- interfaces.go
+++ interfaces.go
@@ -8,17 +8,22 @@ type Rectangle struct {
     width float32
 }
 
+type Point struct{
+    x float32
+    y float32
+}
+
 func (r Rectangle) area() float32 {
     return r.length * r.width
+}
+
+func (r Rectangle) location() Point {
+    return Point{r.length, r.width}
 }
 
 func main() {
     // While struct defines an object, interfaces defines behaviour
     // Declaration of interfaces
-    type Point struct{
-        x float32
-        y float32
-    }
 
     type Shape interface{
         area() float32
@@ -26,6 +31,7 @@ func main() {
     }
 
    myRect := Rectangle{20, 15}
-   fmt.Printf("Area of rectangle with sides %v and %v is: %v", myRect.length, myRect.width, myRect.area())
+   fmt.Printf("Area of rectangle with sides %v and %v is: %v \n", myRect.length, myRect.width, myRect.area())
+   fmt.Printf("Rectangle with sides %v and %v has location: %v \n", myRect.length, myRect.width, myRect.location())
 
 }
