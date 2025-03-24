7635941154089f4c2b7fa81988699e42c4833cae
atodadson <benjamindadson32@gmail.com>
2025-03-24 14:57:29 +0000

    Reading jsons

--- jsons.go
+++ jsons.go
@@ -3,11 +3,16 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 )
 
 type Product struct {
 	Id   int    `json: "id"`
 	Name string `json: "name"`
+}
+
+type Products struct {
+	Products []Product `json: products`
 }
 
 type Response struct {
@@ -19,4 +24,15 @@ func main() {
 	product := Product{}
 	json.Unmarshal([]byte(str), &product)
 	fmt.Println(product)
+
+	file, _ := ioutil.ReadFile("products.json")
+
+	data := Products{}
+
+	_ = json.Unmarshal([]byte(file), &data)
+
+	for i := 0; i < len(data.Products); i++ {
+		fmt.Println("Product Id: ", data.Products[i].Id)
+		fmt.Println("Name: ", data.Products[i].Name)
+	}
 }
