24e78c3af7d6f4621d875adc3053daaebf00c1da
atodadson <benjamindadson32@gmail.com>
2025-03-24 17:01:31 +0000

    Writing to Json

--- jsons.go
+++ jsons.go
@@ -3,7 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"os"
 )
 
 type Product struct {
@@ -13,6 +13,16 @@ type Product struct {
 
 type Products struct {
 	Products []Product `json: products`
+}
+
+type Person struct {
+	Id   int    `json: "id"`
+	Name string `json: "name"`
+	Age  int    `json: "age"`
+}
+
+type Persons struct {
+	Persons []Person `json: persons`
 }
 
 type Response struct {
@@ -25,7 +35,7 @@ func main() {
 	json.Unmarshal([]byte(str), &product)
 	fmt.Println(product)
 
-	file, _ := ioutil.ReadFile("products.json")
+	file, _ := os.ReadFile("products.json")
 
 	data := Products{}
 
@@ -35,4 +45,16 @@ func main() {
 		fmt.Println("Product Id: ", data.Products[i].Id)
 		fmt.Println("Name: ", data.Products[i].Name)
 	}
+
+	Persons_list := Persons{
+		Persons: []Person{
+			{Id: 1, Name: "Kwame", Age: 34},
+			{Id: 2, Name: "Kojo", Age: 23},
+		},
+	}
+
+	jsonData, _ := json.MarshalIndent(Persons_list, "", " ")
+	fmt.Println(string(jsonData))
+
+	os.WriteFile("persons.json", jsonData, 0644)
 }
