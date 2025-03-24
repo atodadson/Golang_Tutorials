fdb93acded126aeff45d644647bb66d88c489d87
atodadson <benjamindadson32@gmail.com>
2025-03-24 14:11:17 +0000

    Reading json string literal

---
+++ jsons.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Product struct {
+	Id   int    `json: "id"`
+	Name string `json: "name"`
+}
+
+type Response struct {
+	Products []Product `json: "products"`
+}
+
+func main() {
+	str := `{"name": "my product", "id": 1}`
+	product := Product{}
+	json.Unmarshal([]byte(str), &product)
+	fmt.Println(product)
+}
