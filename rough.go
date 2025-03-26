b31d6b802dc13e7f58d9c14d0d115d002f0dd6e0
atodadson <benjamindadson32@gmail.com>
2025-03-26 18:22:56 +0000

    Refactored

--- rough.go
+++ rough.go
@@ -8,10 +8,12 @@ func main() {
 	green := "\033[32m"
 	yellow := "\033[33m"
 	reset := "\033[0m"
+	aaa := "checkkls"
 
 	// Print colored text
-	fmt.Println(red + "This is red text!" + reset)
+	fmt.Printf(red + "This is red text!" + "\n" + reset)
 	fmt.Println(green + "This is green text!" + reset)
 	fmt.Println(yellow + "This is yellow text!" + reset)
 	fmt.Println("This is normal text!")
+	fmt.Println(len(aaa))
 }
