a4c925e114df469d5795fe1282efda91e2005a6c
atodadson <benjamindadson32@gmail.com>
2025-03-27 19:15:17 +0000

    High Score & N

--- wordle.go
+++ wordle.go
@@ -28,6 +28,9 @@ func CreateKeyboard() (keyboard map[string]string) {
 	return keyboard
 }
 
+// Score values
+var score_list = map[int]int{1: 13, 2: 8, 3: 5, 4: 3, 5: 2, 6: 1}
+
 func ShowKeyboard(keyboard map[string]string) {
 	row1 := []string{"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"}
 	row2 := []string{"A", "S", "D", "F", "G", "H", "J", "K", "L"}
@@ -57,9 +60,9 @@ func UpdateKeyboard(letter string, colour string, keyboard map[string]string) {
 	}
 }
 
-func Getword() (randword string, wordlist []string) {
+func Getwords() (randword string, wordlist []string) {
 	// Open the file
-	file, err := os.Open("mywords.txt")
+	file, err := os.Open("new_words.txt")
 	if err != nil {
 		fmt.Printf("Error opening file: %v\n", err)
 		return
@@ -104,18 +107,6 @@ func allValuesGreen(m [5]string) bool {
 	}
 	return true
 }
-
-// func ProcessGuess (guess string, word string) (response array){
-// 	for index, char := range guess {
-// 			if word[index] == char {
-// 				response[char] = "Green"
-// 			} else if Inword(char, word) {
-// 				response[char] = "Yellow"
-// 			} else {
-// 				response[char] = "Black"
-// 			}
-// 	}
-// }
 
 // ProcessGuess processes the guess and returns a response
 func ProcessGuess(guess string, word string) [5]string {
@@ -179,10 +170,13 @@ func main() {
 		} else if command == "1" {
 			level := 0
 			var randword string
+			var score = 0
+			var guessed_right = true
 			for true {
 				level++
 				fmt.Println("\n=====================================================================")
-				fmt.Printf("                            LEVEL %v\n", level)
+				fmt.Printf("                            WORD %v\n", level)
+				fmt.Printf("                      CURRENT SCORE: %v\n", score)
 				fmt.Println("=====================================================================")
 
 				// Declaration of variable for each level
@@ -190,27 +184,28 @@ func main() {
 				ShowKeyboard(keyboard)
 				var guess string
 				guesses_left := 7
-				var guessed_right = true
-				randword, _ = Getword()
+				randword, _ = Getwords()
 				randword = strings.ToUpper(randword)
+				fmt.Println("The word is: ", randword)
 
 				if !guessed_right {
+					fmt.Println("Sorry you lost. Please try again")
 					break
 				}
 
 				for i := 1; i < 7; i++ {
 					guesses_left--
-					fmt.Printf("\n\n[%v Guesses left] Guess the word: \n", guesses_left)
+					fmt.Printf("\n[Guess %v/6] Enter word: \n", i)
 					fmt.Scan(&guess)
 					for len(guess) != 5 {
 						fmt.Printf("You entered a %d letter-word. Enter a 5 letter-word\n", len(guess))
-						fmt.Printf("[%v Guesses left] Guess the word: \n", guesses_left)
+						fmt.Printf("\n[Guess %v/6] Enter word: \n", i)
 						fmt.Scan(&guess)
 					}
 					guess = strings.ToUpper(guess)
 					for !reg.MatchString(guess) {
 						fmt.Printf("Your word contains invalid characters\n")
-						fmt.Printf("[%v Guesses left] Guess the word: \n", guesses_left)
+						fmt.Printf("\n[Guess %v/6] Enter word: \n", i)
 						fmt.Scan(&guess)
 					}
 					response := ProcessGuess(guess, randword)
@@ -228,6 +223,7 @@ func main() {
 					if allValuesGreen(response) {
 						fmt.Println("Yeeey, You got it right. That's a great guess. Progress to the next level")
 						guessed_right = true
+						score = score + score_list[i]
 						break
 					}
 					guessed_right = false
