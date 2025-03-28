8306e1ebb5c9397d4d6b548b006db56fbf26b2f4
atodadson <benjamindadson32@gmail.com>
2025-03-28 18:25:38 +0000

    Refactored

--- handling_errors.go
+++ handling_errors.go
@@ -1,53 +1,52 @@
 package main
 
 import (
-"fmt"
-"errors"
-"os"
-"runtime/debug"
-"log"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"runtime/debug"
 )
 
 // Using recover and defer
 func ErrorHandler() {
-    if r := recover(); r != nil {
-        fmt.Printf("Recovering from error: %v \n", r)
-    }
+	if r := recover(); r != nil {
+		fmt.Printf("Recovering from error: %v \n", r)
+	}
 }
 
 // Using panic and recover
 func Divide(dividend float64, divisor float64) (answer float64) {
-    defer ErrorHandler()
-    if divisor == 0 {
-        panic("Can't Divide by 0")
-    }
-    return dividend / divisor
+	defer ErrorHandler()
+	if divisor == 0 {
+		panic("Can't Divide by 0")
+	}
+	return dividend / divisor
 }
-
 
 // Defining errors
 var NumTooSmall = errors.New("Expected a number greater than zero")
 
 func ReturnPositive(no int) (int, error) {
-    if no > 0 {
-        return no, nil
-    } else {
-        return 0, NumTooSmall
-    }
+	if no > 0 {
+		return no, nil
+	} else {
+		return 0, NumTooSmall
+	}
 }
 
 func main() {
-    Divide(20, 0)
-    num, err := ReturnPositive(-12)
-    check := debug.Stack()
-    fmt.Println(check)
+	Divide(20, 0)
+	num, err := ReturnPositive(-12)
+	check := debug.Stack()
+	fmt.Println(check)
 
-    // Error logging
-    f, err := os.OpenFile("logs.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-    if err != nil {
-        log.Println(err)
-    }
-    log.SetOutput(f)
+	// Error logging
+	f, err := os.OpenFile("logs.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	log.SetOutput(f)
 
-    fmt.Printf("Num: %v, Error: %v", num, err)
+	fmt.Printf("Num: %v, Error: %v", num, err)
 }
