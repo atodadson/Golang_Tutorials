d4cf6a6e6ac6c0d63e137ea4136cb15c58d05aa5
atodadson <benjamindadson32@gmail.com>
2025-03-31 08:25:48 +0000

    testing player name input

--- rough2.go
+++ rough2.go
@@ -1,10 +1,23 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "bufio"
+    "os"
+    "strings"
+)
 
 func main() {
     var player string
-    fmt.Println("Enter player name")
-    fmt.Scan(&player)
+    reader := bufio.NewReader(os.Stdin)
+
+    fmt.Println("Enter all names")
+    player, err := reader.ReadString('\n')
+    player = strings.TrimSpace(player)
+    if err != nil {
+        fmt.Println(err)
+    }
+//     fmt.Println("Enter player name")
+//     fmt.ReadString(&player)
     fmt.Println(player)
 }
