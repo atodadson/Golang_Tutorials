50b21fc451955f890d3812f7aec509e210796e51
atodadson <benjamindadson32@gmail.com>
2025-04-04 06:39:42 +0000

    validate high score

--- rough2.go
+++ rough2.go
@@ -2,24 +2,44 @@ package main
 
 import (
     "fmt"
-    "bufio"
+//     "bufio"
     "os"
-    "strings"
+    "encoding/csv"
+    "strconv"
 )
 
-func getPlayerName(message string) (player string) {
-    reader := bufio.NewReader(os.Stdin)
-
-    fmt.Println(message)
-    player, err := reader.ReadString('\n')
-    player = strings.TrimSpace(player)
+func getScoreInfo() (scoreData [][]string, scores []int){
+    csv_file ,err := os.Open("Highscore.csv")
     if err != nil {
         fmt.Println(err)
     }
+    defer csv_file.Close()
+
+    reader := csv.NewReader(csv_file)
+    records, err := reader.ReadAll()
+    for _, record := range records {
+        scoreData = append(scoreData, record)
+        score, _ := strconv.Atoi(record[2])
+        scores = append(scores, score)
+    }
+    scoreData, scores = scoreData[1:], scores[1:]
     return
 }
 
+func isHighScore(newscore int, scores []int) (bool) {
+    for _, score := range scores {
+        if newscore > score {
+            fmt.Printf("newscore: %v greater than score: %v", newscore, score)
+            return true
+        }
+    }
+    return false
+}
+
 func main() {
-    player := getPlayerName("I know your name: ")
-    fmt.Println(player)
+    scoreData, scores := getScoreInfo()
+    b := isHighScore(4, scores)
+
+    fmt.Printf("%v \nSCORE DATA: \n%v \nSCORES: \n %v\n", b, scoreData, scores)
+
 }
